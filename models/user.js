9e01f1b2703d5ea84df49cbd03ba107e8f1049c1
Diky Arga <dikyarga.id@gmail.com>
2017-08-26 00:45:20 +0700

    create model for user and warehouse

---
+++ models/user.js
@@ -0,0 +1,20 @@
+'use strict';
+
+let crypto = require('crypto')
+
+module.exports = function(sequelize, DataTypes) {
+  var User = sequelize.define('User', {
+    name: DataTypes.STRING,
+    username: DataTypes.STRING,
+    password: DataTypes.STRING,
+    email: DataTypes.STRING,
+    identityNumber: DataTypes.STRING
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      }
+    }
+  });
+  return User;
+};
