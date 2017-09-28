9298e8f7daf21d81ff93aaa0745836406d9dae3c
nurul irfan <irfanlongstride@gmail.com>
2017-09-28 12:17:21 +0700

    ubah plain password menjadi salted hash password

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,19 +1,58 @@
 var jwt = require('jsonwebtoken')
 let models = require('../models')
+let crypto = require('crypto');
 
+/**
+ * generates random string of characters i.e salt
+ * @function
+ * @param {number} length - Length of the random string.
+ */
+function genRandomString(length){
+  return crypto.randomBytes(Math.ceil(length/2))
+    .toString('hex') /** convert to hexadecimal format */
+    .slice(0,length);   /** return required number of characters */
+};
+
+/**
+ * hash password with sha512.
+ * @function
+ * @param {string} password - List of required fields.
+ * @param {string} salt - Data to be validated.
+ */
+function hashPassword(password, salt){
+  let hash = crypto.createHmac('sha512', salt); /** Hashing algorithm sha512 */
+  hash.update(password);
+  let value = hash.digest('hex');
+  return value;
+};
+
+function saltHashPassword(userpassword) {
+  let salt = genRandomString(16); /** Gives us salt of length 16 */
+  let hash = hashPassword(userpassword, salt);
+  return {
+    hash: hash,
+    salt: salt
+  }
+}
 
 module.exports = {
   register: function(req, res, next) {
+    passwordData = saltHashPassword(req.body.password)
     models.User.create({
       name: req.body.name,
       email: req.body.email,
       username: req.body.username,
       identityNumber: req.body.identityNumber,
-      password: req.body.password
+      password: passwordData.hash,
+      salt: passwordData.salt
     }).then(user => {
       res.json(user)
     }).catch(err => {
       console.log('Error when trying to register : ', err);
+      res.json({
+        success: false,
+        message: err
+      })
     })
   },
   login: function(req,res,next){
@@ -23,7 +62,8 @@ module.exports = {
       if(!user){
         res.json({success: false, message: 'Authentication failed. User not found.'})
       }else if (user) {
-        if(user.password != req.body.password){
+        let reqPasswordData = hashPassword(req.body.password, user.salt);
+        if(user.password != reqPasswordData){
           res.json({success: false, message: 'Authentication failed. Wrong password.'})
         }else {
           var secret = req.app.get('superSecret')
@@ -38,6 +78,10 @@ module.exports = {
       }
     }).catch(err => {
       console.log('Error when trying to register : ', err);
+      res.json({
+        success: false,
+        message: err
+      })
     })
   }
 }
