14df96b7c1bd6f8519dd1c780c3c2eda93b889b9
sikun <arnazadiputra@gmail.com>
2017-10-17 22:11:07 +0700

    buat relasi dari item ke inner

--- models/item.js
+++ models/item.js
@@ -11,7 +11,8 @@ module.exports = function(sequelize, DataTypes) {
     classMethods: {
       associate: function(models) {
         // associations can be defined here
-        Items.hasOne(models.Category,{foreignKey: 'categoryId'})
+        Item.hasOne(models.Category,{foreignKey: 'categoryId'})
+        Item.belongsTo(models.Inner,{foreignKey: 'itemId'})
       }
     }
   });
