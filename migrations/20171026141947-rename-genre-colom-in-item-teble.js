c6f91ad6400d7432539a4e2b15e9d97bcf1bf716
sikun <arnazadiputra@gmail.com>
2017-10-26 22:53:13 +0700

    buat seeder item , Fixes #23

---
+++ migrations/20171026141947-rename-genre-colom-in-item-teble.js
@@ -0,0 +1,24 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+    */
+    queryInterface.renameColumn('Items', 'genre', 'gender')
+  },
+
+  down: function (queryInterface, Sequelize) {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+  }
+};
