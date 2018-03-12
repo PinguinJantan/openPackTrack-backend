f2a62bac63acf872251906b2e8af15748c4f167f
sikun <arnazadiputra@gmail.com>
2018-03-12 23:08:01 +0700

    add apidoc

--- routes/innerreport.js
+++ routes/innerreport.js
@@ -7,10 +7,10 @@ let aclMiddleware = require('../acl/aclMiddleware');
 router.use(aclMiddleware.isAllowedToAccess('item'))
 
 
-/* GET users listing. */
+/* POST Inner Report Creating Opname. */
 /**
-  * @api {post} api/inner/create Create
-  * @apiGroup Inner
+  * @api {post} api/innerreport/scancarton Opname
+  * @apiGroup Inner Report
   * @apiHeader {String} token token untuk login user
   * @apiHeaderExample {json} Header-Example:
   *     {
@@ -18,36 +18,20 @@ router.use(aclMiddleware.isAllowedToAccess('item'))
   *     }
   * @apiParamExample {json} Request-Example:
   *  {
-  *      "barcode": "1212kj2",
-  *      "cartonId": 7,
-  *      "isInStok": 1,
-  *      "gradeId": "1",
-  *      "sourceId": "2",
+  *      "cartonBarcode": "1212kj2",
+  *      "reportId": 7
   *  }
   * @apiSuccess {Boolean} success true jika berhasil
   * @apiSuccess {string} status "OK" jika berhasil
-  * @apiSuccess {Array} inner array dari inner
-  * @apiParam {string} barcode barcode inner
-  * @apiParam {integer} categoryId id kategori inner
-  * @apiParam {Boolean} isInStok status stok inner
-  * @apiParam {integer} gradeId id grade inner
-  * @apiParam {string} sourceId id source inner
+  * @apiSuccess {Array} innerReport array dari inner
+  * @apiParam {string} cartonBarcode barcode carton
+  * @apiParam {integer} reportId id report
   * @apiSuccessExample {json} success
   *     HTTP/1.1 200 OK
   *    {
-  *      "success": true,
-  *      "status": "OK",
-  *      "inner": {
-  *          "id": 44,
-  *          "barcode": "1212kj2",
-  *          "itemId": 3,
-  *          "cartonId": 7,
-  *          "isInStok": true,
-  *          "gradeId": 1,
-  *          "sourceId": 3,
-  *          "updatedAt": "2017-10-30T12:49:01.130Z",
-  *          "createdAt": "2017-10-30T12:49:01.130Z"
-  *        }
+  *        "success": true,
+           "message": "opname success",
+           "status": "OK"
   *      }
   * @apiErrorExample {json} Internal Server Error
   *     HTTP/1.1 500 Internal Server Error
@@ -58,7 +42,95 @@ router.use(aclMiddleware.isAllowedToAccess('item'))
   *      }
 **/
 router.post('/scancarton',innerReportController.bulkCreateOpname)
+
+/* POST Inner Report Creating. */
+/**
+  * @api {post} api/innerreport/create Create With innerId
+  * @apiGroup Inner Report
+  * @apiHeader {String} token token untuk login user
+  * @apiHeaderExample {json} Header-Example:
+  *     {
+  *       "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHBpcmVzSW5NaW51dGVzIjoxNDQwLCJpYXQiOjE1MDcwMzQwNzJ9.je4md5GBuTSFGNivBaT3Ju7-yjVjkVS99WSIiwk7wA4",
+  *     }
+  * @apiParamExample {json} Request-Example:
+  *  {
+  *      "innerId": "9",
+  *      "reportId": 7
+  *  }
+  * @apiSuccess {Boolean} success true jika berhasil
+  * @apiSuccess {string} status "OK" jika berhasil
+  * @apiSuccess {Array} innerReport array dari inner
+  * @apiParam {string} innerId id inner
+  * @apiParam {integer} reportId id report
+  * @apiSuccessExample {json} success
+  *     HTTP/1.1 200 OK
+  *    {
+        "success": true,
+        "message": "create report success",
+        "status": "OK",
+        "innerReport": {
+            "id": 215,
+            "innerId": 9,
+            "reportId": 13,
+            "updatedAt": "2018-03-12T15:28:28.660Z",
+            "createdAt": "2018-03-12T15:28:28.660Z"
+        }
+  *      }
+  * @apiErrorExample {json} Internal Server Error
+  *     HTTP/1.1 500 Internal Server Error
+  *     {
+  *       success: false,
+  *       status: "ERROR",
+  *       innerReport: null
+  *      }
+**/
 router.post('/create',innerReportController.create)
+/* GET Opname Listing. */
+/**
+  * @api {get} api/innerreport/all List Opname (paginated)
+  * @apiGroup Inner Report
+  * @apiHeader {String} token token untuk login user
+  * @apiHeaderExample {json} Header-Example:
+  *     {
+  *       "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHBpcmVzSW5NaW51dGVzIjoxNDQwLCJpYXQiOjE1MDcwMzQwNzJ9.je4md5GBuTSFGNivBaT3Ju7-yjVjkVS99WSIiwk7wA4",
+  *     }
+  * @apiSuccess {Boolean} success true jika berhasil
+  * @apiSuccess {Array} pagination paginasi
+  * @apiSuccess {string} status "OK" jika berhasil
+  * @apiSuccess {Array} innerReport array dari inner
+  * @apiSuccessExample {json} success
+  *     HTTP/1.1 200 OK
+  *    {
+        "success": true,
+           "message": "Request success",
+           "pagination": {
+               "innerReportTotal": 16,
+               "pageCount": 2,
+               "currentPage": 1,
+               "hasNextPage": true,
+               "hasPrevPage": false
+           },
+           "status": "OK",
+           "innerReport": [
+               {
+                   "id": 212,
+                   "innerId": 7,
+                   "reportId": 13,
+                   "report": {
+                       "id": 13,
+                       "createdAt": "2017-12-12T09:53:00.979Z",
+                       "updatedAt": "2017-12-12T09:53:00.979Z"
+                   }
+               },
+  *      }
+  * @apiErrorExample {json} Internal Server Error
+  *     HTTP/1.1 500 Internal Server Error
+  *     {
+  *       success: false,
+  *       status: "ERROR",
+  *       innerReport: null
+  *      }
+**/
 router.get('/all', innerReportController.all)
 
 module.exports = router;
--- routes/report.js
+++ routes/report.js
@@ -5,6 +5,98 @@ let reportController = require('../controllers/reportController')
 let aclMiddleware = require('../acl/aclMiddleware');
 
 router.use(aclMiddleware.isAllowedToAccess('item'))
+/* POST Create Report. */
+/**
+  * @api {post} api/report/create Create
+  * @apiGroup Report
+  * @apiHeader {String} token token untuk login user
+  * @apiHeaderExample {json} Header-Example:
+  *     {
+  *       "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHBpcmVzSW5NaW51dGVzIjoxNDQwLCJpYXQiOjE1MDcwMzQwNzJ9.je4md5GBuTSFGNivBaT3Ju7-yjVjkVS99WSIiwk7wA4",
+  *     }
+  * @apiParamExample {json} Request-Example:
+  *  {
 
+  *  }
+  * @apiSuccess {Boolean} success true jika berhasil
+  * @apiSuccess {string} status "OK" jika berhasil
+  * @apiSuccess {Array} report
+  * @apiSuccessExample {json} success
+  *     HTTP/1.1 200 OK
+  *    {
+  *       "success": true,
+          "status": "OK",
+          "message": "Create Report Success",
+          "report": {
+              "id": 147,
+              "updatedAt": "2018-03-12T14:35:16.771Z",
+              "createdAt": "2018-03-12T14:35:16.771Z"
+            }
+  *      }
+  * @apiErrorExample {json} Internal Server Error
+  *     HTTP/1.1 500 Internal Server Error
+  *     {
+  *       success: false,
+  *       status: "ERROR",
+  *       report: null
+  *      }
+**/
 router.post('/create',reportController.create)
+/* GET listing Report. */
+/**
+  * @api {get} api/report/all List
+  * @apiGroup Report
+  * @apiHeader {String} token token untuk login user
+  * @apiHeaderExample {json} Header-Example:
+  *     {
+  *       "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHBpcmVzSW5NaW51dGVzIjoxNDQwLCJpYXQiOjE1MDcwMzQwNzJ9.je4md5GBuTSFGNivBaT3Ju7-yjVjkVS99WSIiwk7wA4",
+  *     }
+  * @apiParamExample {json} Request-Example:
+  *  {
+
+  *  }
+  * @apiSuccess {Boolean} success true jika berhasil
+  * @apiSuccess {string} status "OK" jika berhasil
+  * @apiSuccess {Array} report
+  * @apiSuccessExample {json} success
+  *     HTTP/1.1 200 OK
+  *    {
+        "success": true,
+           "message": "Request success",
+           "pagination": {
+               "reportTotal": 136,
+               "pageCount": 14,
+               "currentPage": 1,
+               "hasNextPage": true,
+               "hasPrevPage": false
+           },
+           "report": [
+               {
+                   "id": 17,
+                   "createdAt": "2017-12-12T16:56:14.401Z",
+                   "updatedAt": "2017-12-12T16:56:14.401Z",
+                   "innerReport": [
+                       {
+                           "id": 195,
+                           "innerId": 18,
+                           "createdAt": "2018-02-10T08:00:14.384Z",
+                           "updatedAt": "2018-02-10T08:00:14.384Z",
+                           "inner": {
+                               "barcode": "zxc",
+                               "createdAt": "2017-12-11T17:32:28.054Z",
+                               "updatedAt": "2017-12-11T17:32:28.054Z",
+                               "item": {
+                                   "code": "I122221"
+                               },
+
+  *      }
+  * @apiErrorExample {json} Internal Server Error
+  *     HTTP/1.1 500 Internal Server Error
+  *     {
+  *       success: false,
+  *       status: "ERROR",
+  *       report: null
+  *      }
+**/
+router.get('/all',reportController.all)
 module.exports = router;
