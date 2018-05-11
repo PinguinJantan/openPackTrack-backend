7389c964aee2620f428cd0e872dc48b3604a749e
sikun <arnazadiputra@gmail.com>
2018-05-12 01:24:49 +0700

    fix endpoint repack (Deferrable)

--- controllers/cartonController.js
+++ controllers/cartonController.js
@@ -6,8 +6,8 @@ const Op = sequelize.Op
 let models = require('../models')
 
 module.exports = {
-  create: function(req,res){
-    var result= {
+  create: function(req, res) {
+    var result = {
       success: false,
       status: "ERROR",
       carton: null
@@ -16,12 +16,12 @@ module.exports = {
       barcode: req.body.barcode,
       warehouseId: req.body.warehouseId,
       profileId: req.body.profileId
-    }).then(carton=>{
+    }).then(carton => {
       result.success = true
       result.status = "OK"
       result.carton = carton
       res.json(result)
-    }).catch(err=>{
+    }).catch(err => {
       console.log('Error when trying to create new carton : ', err)
       if (err.errors) {
         result.errors = err.errors
@@ -29,8 +29,8 @@ module.exports = {
       res.json(result)
     })
   },
-  all: function (req, res) {
-    var result= {
+  all: function(req, res) {
+    var result = {
       success: false
     }
     var allowedSort = ['updatedAt', 'barcode']
@@ -50,8 +50,7 @@ module.exports = {
     var text = req.query.search
     console.log(req.query);
     models.Carton.findAndCountAll({
-        include: [
-          {
+        include: [{
             model: models.Profile,
             as: 'profile',
             attributes: {
@@ -60,162 +59,175 @@ module.exports = {
           },
           {
             model: models.Warehouse,
-            as:'warehouse',
+            as: 'warehouse',
             attributes: {
               exclude: ["createdAt", "updatedAt"]
-              }
             }
+          }
         ],
-        attributes:{
-          exclude:["profileId","warehouseId"]
+        attributes: {
+          exclude: ["profileId", "warehouseId"]
         },
         where: {
           $or: [
             // https://stackoverflow.com/questions/33271413/sequelize-or-clause-with-multiple-models
-            sequelize.where(sequelize.col('Carton.barcode'), { $ilike: `%${text}%`}),
+            sequelize.where(sequelize.col('Carton.barcode'), {
+              $ilike: `%${text}%`
+            }),
             // sequelize.where(sequelize.col('profile.type'), { $ilike: `%${text}%`})
           ]
         },
         limit: req.query.limit,
         offset: req.skip,
-        order: [[req.query.sortBy, req.query.sortDirection]]
-      }
-    )
-    .then(data=>{
-      var carton = data.rows
-      var cartonCount = data.count
-      pageCount = Math.ceil(cartonCount / req.query.limit)
-      result.success= true
-      result.status= "OK"
-      result.pagination = {
-        total: cartonCount,
-        pageCount: pageCount,
-        currentPage: req.query.page,
-        hasNextPage: paginate.hasNextPages(req)(pageCount),
-        hasPrevPage: res.locals.paginate.hasPreviousPages
-      }
-      result.cartons = carton
-      res.json(result)
-    })
-    .catch(err=>{
-      console.log('Error when trying to show all carton : ', err);
-      if (err.errors) {
-        result.errors = err.errors
-      }
-      if (err.message) {
-        result.message = err.message
-      }
-      res.json(result)
-    })
+        order: [
+          [req.query.sortBy, req.query.sortDirection]
+        ]
+      })
+      .then(data => {
+        var carton = data.rows
+        var cartonCount = data.count
+        pageCount = Math.ceil(cartonCount / req.query.limit)
+        result.success = true
+        result.status = "OK"
+        result.pagination = {
+          total: cartonCount,
+          pageCount: pageCount,
+          currentPage: req.query.page,
+          hasNextPage: paginate.hasNextPages(req)(pageCount),
+          hasPrevPage: res.locals.paginate.hasPreviousPages
+        }
+        result.cartons = carton
+        res.json(result)
+      })
+      .catch(err => {
+        console.log('Error when trying to show all carton : ', err);
+        if (err.errors) {
+          result.errors = err.errors
+        }
+        if (err.message) {
+          result.message = err.message
+        }
+        res.json(result)
+      })
   },
 
-  detail: function(req, res){
+  detail: function(req, res) {
     var result = {
       success: false,
       carton: null
     }
     models.Carton.find({
-      where: {
-        barcode: req.params.barcode
-      }
-    })
-    .then(carton=>{
-      if (carton) {
-        result.success = true
-        result.carton = carton
+        where: {
+          barcode: req.params.barcode
+        }
+      })
+      .then(carton => {
+        if (carton) {
+          result.success = true
+          result.carton = carton
+          res.json(result)
+        } else {
+          result.message = 'carton not found'
+          res.json(result)
+        }
+      })
+      .catch(err => {
+        if (err.errors) {
+          result.errors = err.errors
+        } else {
+          result.error = err
+        }
         res.json(result)
-      }
-      else {
-        result.message = 'carton not found'
-        res.json(result)
-      }
-    })
-    .catch(err=>{
-      if (err.errors) {
-        result.errors = err.errors
-      }
-      else {
-        result.error = err
-      }
-      res.json(result)
-    })
+      })
   },
 
-  ping: function(req, res){
+  ping: function(req, res) {
     var result = {
       success: false
     }
     models.Carton.find({
-      where: {
-        barcode: req.params.barcode
-      }
-    })
-    .then(carton=>{
-      result.success = true
-      if (carton) {
-        result.exist = true
-      }
-      else {
-        result.exist = false
-      }
-      res.json(result)
-    })
-    .catch(err=>{
-      if(err.errors){
-        result.errors = err.errors
-      }
-      else {
-        result.errors = err
-      }
-      res.json(result)
-    })
+        where: {
+          barcode: req.params.barcode
+        }
+      })
+      .then(carton => {
+        result.success = true
+        if (carton) {
+          result.exist = true
+        } else {
+          result.exist = false
+        }
+        res.json(result)
+      })
+      .catch(err => {
+        if (err.errors) {
+          result.errors = err.errors
+        } else {
+          result.errors = err
+        }
+        res.json(result)
+      })
   },
-  repack: function(req,res){
-    let result={
+  repack: function(req, res) {
+    let result = {
       success: false
     }
-    if(req.body.barcode&&req.body.warehouseId&&req.body.profileId&&req.body.repackCartonId){
-      models.sequelize.transaction(function(t){
+    if (req.body.barcode && req.body.warehouseId && req.body.profileId && req.body.repackCartonId) {
+      models.sequelize.transaction({
+        deferrable: sequelize.Deferrable.SET_DEFERRED
+      }).then(t=>{
         return models.Carton.create({
           barcode: req.body.barcode,
           warehouseId: req.body.warehouseId,
           profileId: req.body.profileId
-        },{
-          transaction:t
+        }, {
+          transaction: t
         }).then((carton) => {
-          let repackCartonId=JSON.parse(req.body.repackCartonId)
+          console.log('disini1');
+          let repackCartonId = JSON.parse(req.body.repackCartonId)
           return models.Inner.findAndCountAll({
-            where:{
-              cartonId:{
-                [Op.in]:repackCartonId
+            where: {
+              cartonId: {
+                [Op.in]: repackCartonId
               }
             }
-          }).then((inners)=>{
-            if(inners.count<12){
-              let innerIds= inners.rows.map(inner=>{
+          }, {
+            transaction: t
+          }).then((inners) => {
+            console.log('disini2');
+            if (inners.count < 12) {
+              let innerIds = inners.rows.map(inner => {
                 return inner.id
               })
-              models.Inner.update(
-                {cartonId: carton.id},
-                {where:{id:{[Op.in]:innerIds}}}
-              ).then(updated=>{
-                result.success= true
-                result.message='repack success'
-                res.json(result)
+              console.log('disini3');
+              return models.Inner.update({
+                cartonId: carton.id
+              }, {
+                where: {
+                  id: {
+                    [Op.in]: innerIds
+                  }
+                }
+              }, {
+                transaction: t
               })
-            }else{
-              result.message='inner more than 12'
-              res.json(result)
+              console.log('disini4');
+            } else {
+              return Promise.reject({message:'inner more than 12'})
             }
           })
         })
+      }).then(inners => {
+        result.success = true
+        result.message = 'repack success'
+        res.json(result)
       }).catch((err) => {
         console.log(err);
-        result.message=err.message
+        result.message = err.message
         res.json(result)
       })
-    }else{
-      result.message='required parameters'
+    } else {
+      result.message = 'required parameters'
       res.status(422).json(result)
     }
   }
